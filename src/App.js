7ff938f8272fec3a4e5d10ba52709b1baf6331b6
Christian Viana <viana2015christian@gmail.com>
2023-07-14 11:49:07 -0300

    Regra para validação de dispositivo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import InstallAppBanner from "./components/InstallAppBanner";
+import Login from "./Page/Login";
 
-function App() {
+const App = () => {
+  const [isAppInstalled, setIsAppInstalled] = useState(false);
+  const [isAndroid, setIsAndroid] = useState(false);
+
+  useEffect(() => {
+    // Verificar se o aplicativo está instalado
+    if (
+      window.matchMedia("(display-mode: standalone)").matches ||
+      window.navigator.standalone
+    ) {
+      setIsAppInstalled(true);
+    }
+
+    // Verificar se é um dispositivo Android
+    if (/Android/i.test(navigator.userAgent)) {
+      setIsAndroid(true);
+    }
+  }, []);
+
   return (
-    <>
-      Academia CityClub
-      <InstallAppBanner />
-    </>
+    <div className="App">
+      {isAndroid ? <InstallAppBanner /> : isAppInstalled && <Login />}
+    </div>
   );
-}
+};
 
 export default App;
