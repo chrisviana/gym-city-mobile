34228146dc54aed2df07830fb1138a4b6e6a91c3
Christian Viana <viana2015christian@gmail.com>
2023-07-18 10:35:08 -0300

    Excluida pagina de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import InstallAppBanner from "./components/InstallAppBanner";
-import { Login } from "./Page/Login";
-
 
 const App = () => {
   return (
     <div className="App">
-      <Login />
     </div>
   );
 };
