d577d9a52e50332f84d429788e5bc0131e98ad7c
Christian Viana <viana2015christian@gmail.com>
2023-07-18 10:44:43 -0300

    Criado pagina de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import InstallAppBanner from "./components/InstallAppBanner";
+import {Login} from './Page/Login'
 
 const App = () => {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 };
